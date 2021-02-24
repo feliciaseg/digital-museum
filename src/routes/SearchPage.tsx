d6ce438b6e18f2f0d21627fc2590e366e8cd9eb6
feliciaseg <felicia.segerholm97@gmail.com>
2021-02-24 16:09:04 +0100

    react-router init

--- src/routes/SearchPage.tsx
+++ src/routes/SearchPage.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-import Header from "../components/header"
+import Header from "../components/header";
+//import { RouteComponentProps } from "react-router-dom"
 
+interface Props {}
 
-interface Props {
-}
+interface State {}
 
-interface State {
-
-}
-
-class SearchPage extends React.Component <Props, State> {
-
-    render() {
-        return <Header h = "8.375rem" c = "#009ad1" ></Header>
-    }
+class SearchPage extends React.Component<Props, State> {
+  render() {
+    return <Header h="8.375rem" c="#009ad1"></Header>;
+  }
 }
 
 export default SearchPage;
