db4deedb565d2ae8106e18e1370c3db3f66922a8
mariahelenanoren <marialinder97@hotmail.com>
2021-02-24 18:22:46 +0100

    created cards

---
+++ src/css.tsx
@@ -0,0 +1,28 @@
+import { CSSProperties } from "react";
+
+export const title: CSSProperties = {
+  lineHeight: "100%",
+  textAlign: "left",
+  fontWeight: "bolder",
+  textTransform: "uppercase",
+};
+
+export const blueBg: CSSProperties = {
+  backgroundColor: "#009AD1",
+};
+
+export const orangeBg: CSSProperties = {
+  backgroundColor: "#FF9C5B",
+};
+
+export const blackBg: CSSProperties = {
+  backgroundColor: "#262730",
+};
+
+export const yellowBg: CSSProperties = {
+  backgroundColor: "#FAFF70",
+};
+
+export const beigeBg: CSSProperties = {
+  backgroundColor: "#E2D0BA",
+};
