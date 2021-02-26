a42422398225329679cb08a7ac3f921b861526d3
feliciaseg <felicia.segerholm97@gmail.com>
2021-02-26 09:20:02 +0100

    searchpage component

--- src/routes/SearchPage.tsx
+++ src/routes/SearchPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Header from "../components/header";
+import SearchBox from "../components/SPsearchBox";
+import SearchResult from "../components/SPsearchResult";
+import searchResult from "../components/SPsearchResult"
 //import { RouteComponentProps } from "react-router-dom"
 
 interface Props {}
@@ -8,7 +11,13 @@ interface State {}
 
 class SearchPage extends React.Component<Props, State> {
   render() {
-    return <Header h="8.375rem" c="#009ad1"></Header>;
+    return (
+      <>
+    <Header h="8.375rem" c="#009ad1"/>
+    <SearchBox searchResult = "REMBRANDT" nmbrOfArtworks = {1024} />
+    <SearchResult/>
+    </>
+    );
   }
 }
 
